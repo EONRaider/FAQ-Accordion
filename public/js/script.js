d1266b12a08a04c4f388115680f5cc745fb74ca5
EONRaider <agsilva@tutanota.com>
2024-01-06 14:03:05 -0300

    Refactor code in OOP

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,19 @@
-let questions = document.querySelectorAll('.card-accordion__question')
+function Accordion() {
+  this.questions = document.querySelectorAll('.card-accordion__question')
+}
 
-questions.forEach(question => {
-  question.addEventListener('click', selectedQuestion => {
-    // Change the question icon styling
-    selectedQuestion.target.classList.toggle('state__minus-icon')
-    // Select the answer to the question and toggle the element's visibility
-    selectedQuestion.target.nextElementSibling.classList.toggle('state__visible-answer')
+Accordion.prototype.setupQuestionButtons = function() {
+  this.questions.forEach(question => {
+    question.addEventListener('click', selectedQuestion => {
+      this.toggleAccordionStates(selectedQuestion.target)
+    })
   })
-})
+}
+
+Accordion.prototype.toggleAccordionStates = function(question) {
+  let answer = question.nextElementSibling
+  answer.classList.toggle('state__visible-answer')
+  question.classList.toggle('state__minus-icon')
+}
+
+new Accordion().setupQuestionButtons()
